46b3d9eec2d52b46776d48f65d620f1638522325
lifeart367-arch <lifeart367@gmail.com>
2026-09-12 12:42:01 +0530

    Update AstraWidgetProvider.java

--- app/src/main/java/com/astrawidgets/AstraWidgetProvider.java
+++ app/src/main/java/com/astrawidgets/AstraWidgetProvider.java
@@ -252,23 +252,11 @@ attachRefreshButton(
                         new RemoteViews(
                                 context.getPackageName(),
                                 R.layout.widget_layout);
-Intent refreshIntent =
-        new Intent(context, AstraWidgetProvider.class);
 
-refreshIntent.setAction(ACTION_REFRESH);
-
-PendingIntent refreshPendingIntent =
-        PendingIntent.getBroadcast(
-                context,
-                widgetId,
-                refreshIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT
-                        | PendingIntent.FLAG_IMMUTABLE);
-
-updatedViews.setOnClickPendingIntent(
-        R.id.widget_refresh,
-        refreshPendingIntent);
-
+attachRefreshButton(
+        context,
+        updatedViews,
+        widgetId);
                 // DATE
 
                 updatedViews.setTextViewText(
